683a2ddf9a0abe407dc7964130acab2e67c10139
Yusuke Tanaka <yusuktan@maguro.dev>
2020-04-29 20:13:20 +0900

    Fix make.sh

--- make.sh
+++ make.sh
@@ -7,19 +7,19 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
+if [[ $(which procon-template 2>&1 > /dev/null; echo $?) -ne 0 ]]; then
+  echo "Unable to find the template extraction script."
+  exit 1
+fi
+
 cargo new $1 --vcs git
 cd $1
 
 mkdir src/bin
 
-SNIPPET_PATH="${XDG_CONFIG_HOME}/coc/ultisnips/rust.snippets"
-TEMPLATE_START_LINE=$(cat ${SNIPPET_PATH} | egrep -n "^snippet template" | cut -d ":" -f 1)
-TEMPLATE_LINES=$(cat ${SNIPPET_PATH} | tail -n +${TEMPLATE_START_LINE} | egrep -n "^endsnippet" | cut -d ":" -f 1)
-TEMPLATE_CONTENT=$(cat ${SNIPPET_PATH} | sed -n "$((TEMPLATE_START_LINE + 1)),$((TEMPLATE_START_LINE + TEMPLATE_LINES - 2))p")
-
 for f in a.rs b.rs c.rs d.rs e.rs f.rs
 do
-  echo "${TEMPLATE_CONTENT}" > src/bin/$f
+  procon-template > src/bin/$f
 done
 
 cargo check
