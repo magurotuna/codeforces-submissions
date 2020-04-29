032220a92ad3539bb742feafe14c77de11107858
Yusuke Tanaka <yusuktan@maguro.dev>
2020-04-29 19:01:52 +0900

    Fix make.sh

--- make.sh
+++ make.sh
@@ -9,90 +9,19 @@ fi
 
 cargo new $1 --vcs git
 cd $1
-git init
 
 mkdir src/bin
 
+SNIPPET_PATH="${XDG_CONFIG_HOME}/coc/ultisnips/rust.snippets"
+TEMPLATE_START_LINE=$(cat ${SNIPPET_PATH} | egrep -n "^snippet template" | cut -d ":" -f 1)
+TEMPLATE_LINES=$(cat ${SNIPPET_PATH} | tail -n +${TEMPLATE_START_LINE} | egrep -n "^endsnippet" | cut -d ":" -f 1)
+TEMPLATE_CONTENT=$(cat ${SNIPPET_PATH} | sed -n "$((TEMPLATE_START_LINE + 1)),$((TEMPLATE_START_LINE + TEMPLATE_LINES - 2))p")
+
 for f in a.rs b.rs c.rs d.rs e.rs f.rs
 do
-  cat <<'EOF' > src/bin/$f
-//! https://github.com/hatoo/competitive-rust-snippets
-//!
-//! MIT License
-//!
-//! Copyright (c) 2018 hatoo
-//!
-//! Permission is hereby granted, free of charge, to any person obtaining a copy
-//! of this software and associated documentation files (the "Software"), to deal
-//! in the Software without restriction, including without limitation the rights
-//! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//! copies of the Software, and to permit persons to whom the Software is
-//! furnished to do so, subject to the following conditions:
-//!
-//! The above copyright notice and this permission notice shall be included in all
-//! copies or substantial portions of the Software.
-//!
-//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//! SOFTWARE.
-#![allow(unused_imports, unused_macros, dead_code)]
-use std::cmp::{max, min, Ordering};
-use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
-use std::io::{stdin, stdout, BufWriter, Write};
-use std::iter::FromIterator;
-mod util {
-    use std::fmt::Debug;
-    use std::io::{stdin, stdout, BufWriter, StdoutLock};
-    use std::str::FromStr;
-    pub fn line() -> String {
-        let mut line: String = String::new();
-        stdin().read_line(&mut line).unwrap();
-        line.trim().to_string()
-    }
-    pub fn chars() -> Vec<char> {
-        line().chars().collect()
-    }
-    pub fn gets<T: FromStr>() -> Vec<T>
-    where
-        <T as FromStr>::Err: Debug,
-    {
-        let mut line: String = String::new();
-        stdin().read_line(&mut line).unwrap();
-        line.split_whitespace()
-            .map(|t| t.parse().unwrap())
-            .collect()
-    }
-    pub fn with_bufwriter<F: FnOnce(BufWriter<StdoutLock>) -> ()>(f: F) {
-        let out = stdout();
-        let writer = BufWriter::new(out.lock());
-        f(writer)
-    }
-}
-macro_rules !get {([$t :ty ] ) =>{{let mut line :String =String ::new () ;stdin () .read_line (&mut line ) .unwrap () ;line .split_whitespace () .map (|t |t .parse ::<$t >() .unwrap () ) .collect ::<Vec <_ >>() } } ;([$t :ty ] ;$n :expr ) =>{(0 ..$n ) .map (|_ |get !([$t ] ) ) .collect ::<Vec <_ >>() } ;($t :ty ) =>{{let mut line :String =String ::new () ;stdin () .read_line (&mut line ) .unwrap () ;line .trim () .parse ::<$t >() .unwrap () } } ;($($t :ty ) ,*) =>{{let mut line :String =String ::new () ;stdin () .read_line (&mut line ) .unwrap () ;let mut iter =line .split_whitespace () ;($(iter .next () .unwrap () .parse ::<$t >() .unwrap () ,) *) } } ;($t :ty ;$n :expr ) =>{(0 ..$n ) .map (|_ |get !($t ) ) .collect ::<Vec <_ >>() } ;($($t :ty ) ,*;$n :expr ) =>{(0 ..$n ) .map (|_ |get !($($t ) ,*) ) .collect ::<Vec <_ >>() } ;}
-macro_rules !debug {($($a :expr ) ,*) =>{eprintln !(concat !($(stringify !($a ) ," = {:?}, " ) ,*) ,$($a ) ,*) ;} }
-const BIG_STACK_SIZE: bool = true;
-fn main() {
-    use std::thread;
-    if BIG_STACK_SIZE {
-        thread::Builder::new()
-            .stack_size(32 * 1024 * 1024)
-            .name("solve".into())
-            .spawn(solve)
-            .unwrap()
-            .join()
-            .unwrap();
-    } else {
-        solve();
-    }
-}
-fn solve() {
-    todo!();
-}
-EOF
+  echo "${TEMPLATE_CONTENT}" > src/bin/$f
 done
 
 cargo check
+
+echo "Finished!"
